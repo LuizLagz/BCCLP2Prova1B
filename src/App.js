e1318da5d66b9834174aa3ae7465f77b21c64efb
LuizLagz <luizfeliperosario@gmail.com>
2023-10-02 10:50:41 -0300

    commit 2

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
+import { Container } from "react-bootstrap";
 import GradeProdutos from "./componentes/GradeProdutos";
 import BarraBusca from "./templates/BarraBusca";
+import TabelaCarrinho from "./componentes/TabelaCarrinho"
 import Cabecalho from "./templates/Cabecalho";
 import { useEffect, useState } from "react";
 
@@ -14,6 +16,9 @@ function App() {
 
   const [produtos, setProdutos] = useState([]);
   const [listaCarrinho, setListaCarrinho] = useState([]);
+  const [exibirFormulario, setExibirFormulario] = useState(true);
+
+
   /*var lista;
   if(lista = localStorage.getItem("carrinho") !== null){
       setListaCarrinho(JSON.parse(lista));
@@ -24,10 +29,14 @@ function App() {
 
 
   return (
+  
     <div className="App">
-      <Cabecalho />
-      <BarraBusca setListaCarrinho={setListaCarrinho} listaCarrinho={listaCarrinho}/>
-      <GradeProdutos listaProdutos={produtos} setListaCarrinho={setListaCarrinho} listaCarrinho={listaCarrinho}/>
+      exibirFormulario?
+        <Cabecalho />
+        <BarraBusca setListaCarrinho={setListaCarrinho} listaCarrinho={listaCarrinho}/>
+        <GradeProdutos listaProdutos={produtos} setListaCarrinho={setListaCarrinho} listaCarrinho={listaCarrinho}/>
+        :
+        <TabelaCarrinho listaCarrinho={listaCarrinho}/>
     </div>
   );
 }
